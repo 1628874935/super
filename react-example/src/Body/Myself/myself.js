737797d2dde618ada0766c25a2578e37de7c2798
your name <1628874935@qq.com>
2019-12-29 17:40:58 +0800

    last but not least

--- react-example/src/Body/Myself/myself.js
+++ react-example/src/Body/Myself/myself.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './myself.css'
-import { Menu, Icon,Calendar, Drawer, Radio, Button, Col, Row, Input, Card, DatePicker,Carousel} from 'antd';
+import { Menu, Icon,Calendar, Drawer, Radio, Button, Col, Row, Input, Card, DatePicker,Carousel, message} from 'antd';
 import Picture20 from './../../pictrue/20.jpg'
 import Picture21 from './../../pictrue/21.jpg'
 import Picture22 from './../../pictrue/22.jpg'
@@ -16,6 +16,25 @@ const { Meta } = Card;
 const { SubMenu } = Menu;
 
 export default class myselfPage extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            username: window.localStorage.getItem('username')
+         
+        }
+    }
+
+
+   changeValue=(e)=>{
+        this.setState({
+            [e.target.name]:e.target.value
+        })
+    
+   }
+
+
+
+
 
     state = {
         current: 'mail',
@@ -28,15 +47,7 @@ export default class myselfPage extends React.Component{
         });
       };
 
-      constructor(props){
-        super(props);
-        this.state={
-            username: window.localStorage.getItem('username')
-         
-         
-        }
-    }
-
+     
   
 
 
@@ -50,7 +61,50 @@ export default class myselfPage extends React.Component{
 
     }
 
-    
+    upload = e=>{
+            //XHR
+        var xhr = new XMLHttpRequest()
+        var data ={
+            "adress":this.state.adress,
+            "postman":this.state.postman,
+            "name":this.state.name,
+            "phonenumber":this.state.phonenumber
+
+        }
+
+        //open连接
+        xhr.open("post","/user/register")
+        //配置响应函数
+        xhr.onreadystatechange=function(props){
+            if(xhr.readyState==4){
+                if(xhr.status==200){
+                    console.log(xhr.responseText)
+                    var result = JSON.parse(xhr.responseText)
+                    if(result.state==2){
+                        
+                    message.info("用户名已经存在")
+                    }else{
+                        
+                    message.info("注册成功")
+                    this.props.histroy.push("/homepage")
+                    }
+                }
+
+            }
+            else{
+                message.info(xhr.status)
+            }
+
+        }
+        //发送数据
+        xhr.setRequestHeader('content-type','application/json');
+
+        xhr.send(JSON.stringify(data))
+
+        //fetch
+
+        //axios
+    }
    
     
 
@@ -138,19 +192,19 @@ export default class myselfPage extends React.Component{
                       </div>
                       <div>
                           <Button>地址信息：</Button>
-                          <Input placeholder="地址信息" size='large'/>
+                          <Input placeholder="地址信息" size='large' name="adress" value={this.state.adress}  onChange={e=>this.changeValue(e)}/>
                       </div>
                       <div>
                           <Button>邮政编码：</Button>
-                          <Input placeholder="邮政编码" size='large'/>
+                          <Input placeholder="邮政编码" size='large' name="postman" value={this.state.postman}  onChange={e=>this.changeValue(e)}/>
                       </div>
                       <div>
                           <Button>收货人姓名：</Button>
-                          <Input placeholder="收货人姓名" size='large'/>
+                          <Input placeholder="收货人姓名" size='large' name="name" value={this.state.name }  onChange={e=>this.changeValue(e)}/>
                       </div>
                       <div>
                           <Button>手机号：</Button>
-                          <Input placeholder="手机号" size='large'/>
+                          <Input placeholder="手机号" size='large' name="phonenumber" value={this.state.phonenumber}  onChange={e=>this.changeValue(e)}/>
                       </div>
                         <div className={myselfCss.myself23}>
                             <Radio>设为默认地址</Radio>
@@ -159,7 +213,7 @@ export default class myselfPage extends React.Component{
                 </div>
 
                 <div className={myselfCss.myself3}>
-                <Button type='primary' size='large'>保存</Button>
+                <Button type='primary' size='large' onClick={this.upload}>保存</Button>
                 </div>
 
 
--- react-example/src/Enrol/enrol.js
+++ react-example/src/Enrol/enrol.js
@@ -1,9 +1,69 @@
 import React from 'react';
 import './enrol.css'
-import {Button,Icon,Tooltip,Input} from 'antd'
+import {Button,Icon,Tooltip,Input,message} from 'antd'
 var loginCss = require('./enrol.css');
 
 export default class EnrolPage extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+           
+        }
+    }
+
+
+   changeValue=(e)=>{
+        this.setState({
+            [e.target.name]:e.target.value
+        })
+    
+   }
+
+        upload = e=>{
+            //XHR
+        var xhr = new XMLHttpRequest()
+        var data ={
+            "adress":this.state.adress,
+            "postman":this.state.postman,
+            "name":this.state.name,
+            "phonenumber":this.state.phonenumber
+
+        }
+
+        //open连接
+        xhr.open("post","/user/register")
+        //配置响应函数
+        xhr.onreadystatechange=function(props){
+            if(xhr.readyState==4){
+                if(xhr.status==200){
+                    console.log(xhr.responseText)
+                    var result = JSON.parse(xhr.responseText)
+                    if(result.state==2){
+                        
+                    message.info("用户名已经存在")
+                    }else{
+                        
+                    message.info("注册成功")
+                    this.props.histroy.push("/homepage")
+                    }
+                }
+
+            }
+            else{
+                message.info(xhr.status)
+            }
+
+        }
+        //发送数据
+        xhr.setRequestHeader('content-type','application/json');
+
+        xhr.send(JSON.stringify(data))
+
+        //fetch
+
+        //axios
+        }
+
 
     render(){
         return(
@@ -15,12 +75,39 @@ export default class EnrolPage extends React.Component{
 
 
             <div className={loginCss.land2}>
+            <div  className={loginCss.land21}>
+                    <Tooltip placement="topLeft" title="Prompt Text">
+                        <Button >姓名</Button>
+                    </Tooltip>
+                    <div className={loginCss.land22}>
+                    <Input.TextArea placeholder="中文汉字" name="username" value={this.state.username}  onChange={e=>this.changeValue(e)}/>
+                    </div>
+                    
+                </div>
+                <div  className={loginCss.land21}>
+                    <Tooltip placement="topLeft" title="Prompt Text">
+                        <Button>性别</Button>
+                    </Tooltip>
+                    <div className={loginCss.land22}>
+                    <Input.TextArea placeholder="男/女" name="sex" value={this.state.sex}  onChange={e=>this.changeValue(e)}/>
+                    </div>
+                    
+                </div>
+                <div  className={loginCss.land21}>
+                    <Tooltip placement="topLeft" title="Prompt Text">
+                        <Button>年龄</Button>
+                    </Tooltip>
+                    <div className={loginCss.land22}>
+                    <Input.TextArea placeholder="1——125" name="age" value={this.state.age}  onChange={e=>this.changeValue(e)}/>
+                    </div>
+                    
+                </div>
                 <div  className={loginCss.land21}>
                     <Tooltip placement="topLeft" title="Prompt Text">
                         <Button>手机号</Button>
                     </Tooltip>
                     <div className={loginCss.land22}>
-                    <Input.TextArea placeholder="11位数字" />
+                    <Input.TextArea placeholder="11位数字" name="phonenumber" value={this.state.phonenumber}  onChange={e=>this.changeValue(e)}/>
                     </div>
                     
                 </div>
@@ -29,7 +116,7 @@ export default class EnrolPage extends React.Component{
                         <Button>设置密码</Button>
                     </Tooltip>
                     <div className={loginCss.land22}>
-                    <Input.Password placeholder="不能有特殊字符"/>
+                    <Input.Password placeholder="不能有特殊字符" name="password" value={this.state.Password}  onChange={e=>this.changeValue(e)}/>
                     </div>
                 </div>
             </div>
@@ -38,7 +125,7 @@ export default class EnrolPage extends React.Component{
                            
                         <div className={loginCss.land23}>
                             <a>
-                            <Button shape='round' type='primary' href='/login'>
+                            <Button shape='round' type='primary' href='/login' onClick={this.upload}>
                                 注册</Button>
                             </a>
                         </div>
